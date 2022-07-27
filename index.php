32d9538c98dfe0f35f15e02d2250d4246db54c8f
Neo-Machina <andreistatache1997@gmail.com>
2022-07-27 17:21:28 +0200

    ALPHA EXERCISE

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<!DOCTYPE html>
+<?php 
+    class Movie {
+        public $title;
+
+        public $director;
+
+        public $releasedDate;
+        
+        public $language = 'English';
+
+        public $overview;
+
+        public function __construct($_title, $_director) {
+            $this->title = $_title;
+            $this->director = $_director;
+        }
+
+        public function getOverviewMovie($_overview) {
+            return $this->overview = $_overview;
+        }
+    }
+
+    $taxiDriver = new Movie('Taxi Driver', 'Martin Scorsese');
+    $taxiDriver->releasedDate = 'February 9, 1976';
+    $taxiDriver->getOverviewMovie('Travis Bickle (De Niro) is an insomniac ex-marine who works the night shift as a taxi driver in 1970s New York. Chronically lonely and socially inept, he attempts to woo an attractive presidential campaign worker (Shepherd), but his misguided ideas about romance fall flat. Increasingly paranoid, Bickle fancies himself as the decaying city\'s saviour, culminating in a political assassination plot and the violent \'rescue\' of a child prostitute (Foster). A landmark portrait of psychosis from director Martin Scorsese and screenwriter Paul Schrader.');
+    var_dump($taxiDriver);
+
+    $drive = new Movie('Drive', 'Nicolas Winding Refn');
+    $drive->releasedDate = 'May 20, 2011';
+    $drive->getOverviewMovie('This film is about a \'driver\' (Ryan Gosling) who works at a garage fixing cars, but also drives stunt cars in movies. He also does some getaway driving to earn extra cash. He gets close with a woman, Irene, and her child, who is his neighbor. Her husband gets released from prison and and owes some people money, so the \'driver\' ends up doing a job with him. But it goes wrong and the \'driver\' ends up in a crime he didn\'t want to be involved in. Can he protect the people that he now cares about?');
+    var_dump($drive);
+
+    $nightcrawler = new Movie('Nightcrawler', 'Dan Gilroy');
+    $nightcraweler->releasedDate = 'October 31, 2014';
+    $nightcraweler->getOverviewMovie('NIGHTCRAWLER is a thriller set in the nocturnal underbelly of contemporary Los Angeles. Jake Gyllenhaal stars as Lou Bloom, a driven young man desperate for work who discovers the high-speed world of L.A. crime journalism. Finding a group of freelance camera crews who film crashes, fires, murder and other mayhem, Lou muscles into the cut-throat, dangerous realm of nightcrawling - where each police siren wail equals a possible windfall and victims are converted into dollars and cents. Aided by Rene Russo as Nina, a veteran of the blood-sport that is local TV news, Lou blurs the line between observer and participant to become the star of his own story.');
+    var_dump($nightcraweler);
+?>
+
+
+<!DOCTYPE html>     
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,6 +48,14 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <h1>OMG HE'S LITERALLY ME MOVIES</h1>
+
+    <div class="row">
+        <div class="col">
+            <div>
+                TITLE: <?php echo $drive->title; ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
