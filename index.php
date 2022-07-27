e64e1b2ed1343f99df1b595b48e48a0725553027
Neo-Machina <andreistatache1997@gmail.com>
2022-07-28 00:31:56 +0200

    foreach version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
         public $director;
 
-        public $releasedDate;
+        public $released_date;
         
         public $language = 'English';
 
@@ -25,15 +25,15 @@
     }
 
     $taxiDriver = new Movie('Taxi Driver', 'Martin Scorsese');
-    $taxiDriver->releasedDate = 'February 9, 1976';
+    $taxiDriver->released_date = 'February 9, 1976';
     $taxiDriver->setOverviewMovie('Travis Bickle (De Niro) is an insomniac ex-marine who works the night shift as a taxi driver in 1970s New York. Chronically lonely and socially inept, he attempts to woo an attractive presidential campaign worker (Shepherd), but his misguided ideas about romance fall flat. Increasingly paranoid, Bickle fancies himself as the decaying city\'s saviour, culminating in a political assassination plot and the violent \'rescue\' of a child prostitute (Foster). A landmark portrait of psychosis from director Martin Scorsese and screenwriter Paul Schrader.');
 
     $drive = new Movie('Drive', 'Nicolas Winding Refn');
-    $drive->releasedDate = 'May 20, 2011';
+    $drive->released_date = 'May 20, 2011';
     $drive->setOverviewMovie('This film is about a \'driver\' (Ryan Gosling) who works at a garage fixing cars, but also drives stunt cars in movies. He also does some getaway driving to earn extra cash. He gets close with a woman, Irene, and her child, who is his neighbor. Her husband gets released from prison and and owes some people money, so the \'driver\' ends up doing a job with him. But it goes wrong and the \'driver\' ends up in a crime he didn\'t want to be involved in. Can he protect the people that he now cares about?');
 
     $nightcrawler = new Movie('Nightcrawler', 'Dan Gilroy');
-    $nightcrawler->releasedDate = 'October 31, 2014';
+    $nightcrawler->released_date = 'October 31, 2014';
     $nightcrawler->setOverviewMovie('NIGHTCRAWLER is a thriller set in the nocturnal underbelly of contemporary Los Angeles. Jake Gyllenhaal stars as Lou Bloom, a driven young man desperate for work who discovers the high-speed world of L.A. crime journalism. Finding a group of freelance camera crews who film crashes, fires, murder and other mayhem, Lou muscles into the cut-throat, dangerous realm of nightcrawling - where each police siren wail equals a possible windfall and victims are converted into dollars and cents. Aided by Rene Russo as Nina, a veteran of the blood-sport that is local TV news, Lou blurs the line between observer and participant to become the star of his own story.');
 ?>
 
@@ -59,25 +59,11 @@
                         <img src="img/taxi_driver.jpg" alt="taxidriver">
 
                         <div class="movie_info">
-                            <div>
-                                <span>TITLE</span>: <?php echo $taxiDriver->title; ?>
-                            </div>
-
-                            <div>
-                                <span>DIRECTOR</span>: <?php echo $taxiDriver->director; ?>
-                            </div>
-                            
-                            <div>
-                                <span>RELEASED DATE</span>: <?php echo $taxiDriver->releasedDate; ?>
-                            </div>
-
-                            <div>
-                                <span>LANGUAGE</span>: <?php echo $taxiDriver->language; ?>
-                            </div>
-
-                            <div>
-                                <span>OVERVIEW</span>: <?php echo $taxiDriver->overview; ?>
-                            </div>
+                            <?php foreach($taxiDriver as $key => $value) { ?>
+                                <div>
+                                    <span><?php echo $key ?></span>: <?php echo $value . '<br>'; ?>
+                                </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
@@ -87,25 +73,11 @@
                         <img src="img/drive.jpg" alt="drive">
 
                         <div class="movie_info">
-                            <div>
-                                <span>TITLE</span>: <?php echo $drive->title; ?>
-                            </div>
-
-                            <div>
-                                <span>DIRECTOR</span>: <?php echo $drive->director; ?>
-                            </div>
-                            
-                            <div>
-                                <span>RELEASED DATE</span>: <?php echo $drive->releasedDate; ?>
-                            </div>
-
-                            <div>
-                                <span>LANGUAGE</span>: <?php echo $drive->language; ?>
-                            </div>
-
-                            <div>
-                                <span>OVERVIEW</span>: <?php echo $drive->overview; ?>
-                            </div>
+                            <?php foreach($drive as $key => $value) { ?>
+                                <div>
+                                    <span><?php echo $key ?></span>: <?php echo $value . '<br>'; ?>
+                                </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
@@ -115,25 +87,11 @@
                         <img src="img/nightcrawler.jpg" alt="nightcrawler">
 
                         <div class="movie_info">
-                            <div>
-                                <span>TITLE</span>: <?php echo $nightcrawler->title; ?>
-                            </div>
-
-                            <div>
-                                <span>DIRECTOR</span>: <?php echo $nightcrawler->director; ?>
-                            </div>
-                            
-                            <div>
-                               <span>RELEASED DATE</span>: <?php echo $nightcrawler->releasedDate; ?>
-                            </div>
-
-                            <div>
-                                <span>LANGUAGE</span>: <?php echo $nightcrawler->language; ?>
-                            </div>
-
-                            <div>
-                                <span>OVERVIEW</span>: <?php echo $nightcrawler->overview; ?>
-                            </div>
+                            <?php foreach($nightcrawler as $key => $value) { ?>
+                                <div>
+                                    <span><?php echo $key ?></span>: <?php echo $value . '<br>'; ?>
+                                </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
