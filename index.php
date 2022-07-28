9def13babffe1db1e78e2377aed6c94286e8e919
Neo-Machina <andreistatache1997@gmail.com>
2022-07-28 12:06:26 +0200

    foreach col version

--- index.php
+++ index.php
@@ -10,9 +10,12 @@
 
         public $overview;
 
-        public function __construct($_title, $_director) {
+        public $poster;
+
+        public function __construct($_title, $_director, $_poster) {
             $this->title = $_title;
             $this->director = $_director;
+            $this->poster = $_poster;
         }
 
         public function setOverviewMovie($_overview) {
@@ -23,18 +26,26 @@
             return $this->overview;
         }
     }
+    
+    $movieArray = [];
 
-    $taxiDriver = new Movie('Taxi Driver', 'Martin Scorsese');
+    $taxiDriver = new Movie('Taxi Driver', 'Martin Scorsese', 'taxi_driver.jpg');
     $taxiDriver->released_date = 'February 9, 1976';
     $taxiDriver->setOverviewMovie('Travis Bickle (De Niro) is an insomniac ex-marine who works the night shift as a taxi driver in 1970s New York. Chronically lonely and socially inept, he attempts to woo an attractive presidential campaign worker (Shepherd), but his misguided ideas about romance fall flat. Increasingly paranoid, Bickle fancies himself as the decaying city\'s saviour, culminating in a political assassination plot and the violent \'rescue\' of a child prostitute (Foster). A landmark portrait of psychosis from director Martin Scorsese and screenwriter Paul Schrader.');
 
-    $drive = new Movie('Drive', 'Nicolas Winding Refn');
+    $movieArray [] = $taxiDriver;
+
+    $drive = new Movie('Drive', 'Nicolas Winding Refn', 'drive.jpg');
     $drive->released_date = 'May 20, 2011';
     $drive->setOverviewMovie('This film is about a \'driver\' (Ryan Gosling) who works at a garage fixing cars, but also drives stunt cars in movies. He also does some getaway driving to earn extra cash. He gets close with a woman, Irene, and her child, who is his neighbor. Her husband gets released from prison and and owes some people money, so the \'driver\' ends up doing a job with him. But it goes wrong and the \'driver\' ends up in a crime he didn\'t want to be involved in. Can he protect the people that he now cares about?');
 
-    $nightcrawler = new Movie('Nightcrawler', 'Dan Gilroy');
+    $movieArray [] = $drive;
+
+    $nightcrawler = new Movie('Nightcrawler', 'Dan Gilroy', 'nightcrawler.jpg');
     $nightcrawler->released_date = 'October 31, 2014';
     $nightcrawler->setOverviewMovie('NIGHTCRAWLER is a thriller set in the nocturnal underbelly of contemporary Los Angeles. Jake Gyllenhaal stars as Lou Bloom, a driven young man desperate for work who discovers the high-speed world of L.A. crime journalism. Finding a group of freelance camera crews who film crashes, fires, murder and other mayhem, Lou muscles into the cut-throat, dangerous realm of nightcrawling - where each police siren wail equals a possible windfall and victims are converted into dollars and cents. Aided by Rene Russo as Nina, a veteran of the blood-sport that is local TV news, Lou blurs the line between observer and participant to become the star of his own story.');
+
+    $movieArray [] = $nightcrawler;
 ?>
 
 
@@ -54,47 +65,35 @@
             <h1>OMG HE'S LITERALLY ME MOVIES</h1>
 
             <div class="row">
-                <div class="col">
-                    <div class="card">
-                        <img src="img/taxi_driver.jpg" alt="taxidriver">
-
-                        <div class="movie_info">
-                            <?php foreach($taxiDriver as $key => $value) { ?>
+                <?php foreach($movieArray as $movie) { ?>
+                    <div class="col">
+                        <div class="card">
+                            <img src="img/<?php echo $movie->poster ?>" alt="<?php echo $movie->title ?>">
+                            
+                            <div class="movie_info">
                                 <div>
-                                    <span><?php echo $key ?></span>: <?php echo $value . '<br>'; ?>
+                                    <span>TITLE</span>: <?php echo $movie->title; ?>
                                 </div>
-                            <?php } ?>
+
+                                <div>
+                                    <span>DIRECTOR</span>: <?php echo $movie->director; ?>
+                                </div>
+
+                                <div>
+                                    <span>RELEASED DATE</span>: <?php echo $movie->released_date; ?>
+                                </div>
+
+                                <div>
+                                    <span>LANGUAGE</span>: <?php echo $movie->language; ?>
+                                </div>
+
+                                <div>
+                                    <span>OVERVIEW</span>: <?php echo $movie->overview; ?>
+                                </div>                   
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="col">
-                    <div class="card">
-                        <img src="img/drive.jpg" alt="drive">
-
-                        <div class="movie_info">
-                            <?php foreach($drive as $key => $value) { ?>
-                                <div>
-                                    <span><?php echo $key ?></span>: <?php echo $value . '<br>'; ?>
-                                </div>
-                            <?php } ?>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col">
-                    <div class="card">
-                        <img src="img/nightcrawler.jpg" alt="nightcrawler">
-
-                        <div class="movie_info">
-                            <?php foreach($nightcrawler as $key => $value) { ?>
-                                <div>
-                                    <span><?php echo $key ?></span>: <?php echo $value . '<br>'; ?>
-                                </div>
-                            <?php } ?>
-                        </div>
-                    </div>
-                </div>
+                <?php } ?>  
             </div>
         </div>
     </main>
